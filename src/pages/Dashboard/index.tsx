1f90e0029ad4163f66d5beac30e6581bbaa541ea
Luiz Chaves <lucachaves@gmail.com>
2020-04-22 19:53:16 -0300

    estilizando dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,70 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashborad: React.FC = () => {
-  return <Title>Explore repositório no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositório no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="test">
+          <img
+            src="https://avatars3.githubusercontent.com/u/205412?s=460&u=6472e12aeadbb1d8d673e9a402ed29632a8d3aaf&v=4"
+            alt="Luiz Carlos"
+          />
+          <div>
+            <strong>lucachaves/barn-dashboard</strong>
+            <p>
+              The Barn Dashboard is a Bioeconomy Project developed at Hamk
+              Smart.
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://avatars3.githubusercontent.com/u/205412?s=460&u=6472e12aeadbb1d8d673e9a402ed29632a8d3aaf&v=4"
+            alt="Luiz Carlos"
+          />
+          <div>
+            <strong>lucachaves/barn-dashboard</strong>
+            <p>
+              The Barn Dashboard is a Bioeconomy Project developed at Hamk
+              Smart.
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://avatars3.githubusercontent.com/u/205412?s=460&u=6472e12aeadbb1d8d673e9a402ed29632a8d3aaf&v=4"
+            alt="Luiz Carlos"
+          />
+          <div>
+            <strong>lucachaves/barn-dashboard</strong>
+            <p>
+              The Barn Dashboard is a Bioeconomy Project developed at Hamk
+              Smart.
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashborad;
